bb5960d4a6c09c9e8296f7d65a750206ff4f8192
William Douglas <william@rocklandscientific.com>
2023-04-23 15:39:17 -0700

    Clean up unwanted settings

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -2,8 +2,3 @@ require('user.remap')
 require('user.lazy')
 require('user.set')
 
---- Set colorscheme
---vim.opt.termguicolors = true
---vim.cmd.colorscheme('tokyonight')
---vim.cmd.colorscheme('catppuccin')
-
